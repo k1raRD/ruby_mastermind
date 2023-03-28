b92497274c917b9904c3fbd3e2bde9e8448266d2
k1rard <Alexanderyt777@gmail.com>
2023-03-28 19:31:29 -0400

    Finishing the game

---
+++ main.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require './game'
+require './display'
+require './human_solver'
+require './computer_solver'
+require './game_logic'
+require './text_content'
+require './text_instructions'
+
+Game.new.play
